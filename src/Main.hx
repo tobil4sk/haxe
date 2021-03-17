bd9eeb7d6006155e72a4cf0dc72ec72d1af463eb
tobil4sk <tobil4sk@outlook.com>
2021-03-17 20:33:34 +0000

    Move lock file loading into separate function

--- src/Main.hx
+++ src/Main.hx
@@ -11,7 +11,6 @@ class Main {
 
 	final packageManager:PackageManager;
 
-	final lockData:LockFormat;
 
 	/** Array of haxec calls that will be run at the end **/
 	final builds:Array<Build>;
@@ -22,8 +21,6 @@ class Main {
 
 		packageManager = new PackageManager();
 
-		lockData = [];
-
 		builds = [];
 	}
 
@@ -33,34 +30,7 @@ class Main {
 	function run(){
 		// expand all .hxml files
 
-		// open lock files
-
-		// local one
-		loadLockFile(LOCKFILE);
-
-		// lock file given as argument
-		if (args.contains("--lock-file")) {
-			var index = args.indexOf("--lock-file");
-			var filePath = args.splice(index, 2)[1];
-			trace(index, filePath);
-
-			// if given as an argument, it is not optional so throw error if it is not found
-			loadLockFile(filePath, false);
-		}
-
-		// global lock file
-		var globalLockPath = Sys.getEnv("HAXELIB_OVERRIDE_PATH");
-		if (globalLockPath == null) {
-			var home = Sys.getEnv("HOME");
-			if (home == null)
-				// Windows
-				home = Sys.getEnv("USERPROFILE");
-		}
-		var globalFile = Path.join([globalLockPath, GLOBAL_LOCKFILE]);
-
-		trace(globalFile);
-
-		loadLockFile(globalFile);
+		final lockData = loadLockFiles();
 
 
 		for (i => j in lockData){
@@ -85,20 +55,53 @@ class Main {
 		}
 	}
 
-	/**
-		Parses a lockfile and if new values are set then overrides old ones
-	 */
-	function loadLockFile(path:String, optional = true):Void {
-		if (sys.FileSystem.exists(path)) {
-			var content = haxe.Json.parse(sys.io.File.getContent('./${LOCKFILE}'));
-			var lock = LockFormat.load(content);
-			for(lib in lock.keys())
-				lockData[lib] = lock[lib];
-			return;
+	function loadLockFiles():LockFormat{
+		// open lock files
+		var lockData:LockFormat = [];
+
+		/** Parses a lockfile and if new values are set then overrides old ones */
+		function loadLockFile(path:String, optional = true):Void {
+			if (sys.FileSystem.exists(path)) {
+				var content = haxe.Json.parse(sys.io.File.getContent(path));
+				var lock = LockFormat.load(content);
+				for (lib in lock.keys())
+					lockData[lib] = lock[lib];
+				return;
+			}
+
+			if (!optional)
+				throw 'Lock file at ${path} not found';
 		}
 
-		if(!optional)
-			throw 'Lock file at ${path} not found';
+		// local one
+		loadLockFile(LOCKFILE);
+
+		// lock file given as argument
+		if (args.contains("--lock-file")) {
+			var index = args.indexOf("--lock-file");
+			var filePath = args.splice(index, 2)[1];
+			trace(index, filePath);
+
+			// if given as an argument, it is not optional so throw error if it is not found
+			loadLockFile(filePath, false);
+		}
+
+		// global lock file
+		var globalLockPath = Sys.getEnv("HAXELIB_OVERRIDE_PATH");
+		if (globalLockPath == null) {
+			var home = Sys.getEnv("HOME");
+			if (home == null)
+				// Windows
+				home = Sys.getEnv("USERPROFILE");
+			globalLockPath = home;
+		}
+		var globalFile = Path.join([globalLockPath, GLOBAL_LOCKFILE]);
+
+		trace(globalFile);
+
+		loadLockFile(globalFile);
+
+		return lockData;
 	}
 
 	/** entry point **/
